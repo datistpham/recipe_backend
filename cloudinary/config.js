69073a4fb568b3ec803924967045db2e41d9168a
datispham <datistpham@gmail.com>
2022-06-07 15:55:32 +0700

    up

--- cloudinary/config.js
+++ cloudinary/config.js
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from "cloudinary"
 
 export default cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "cockbook",
-    api_key: process.env.CLOUDINARY_API_KEY || "362125891568421",
-    api_secret: process.env.CLOUDINARY_API_SECRET || "kR3bk36ysLWcYuKLy-MN9otXogM",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 })
